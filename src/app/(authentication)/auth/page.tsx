a599e45b0ac2e78e0e82145870cc2347adfd6423
Boominathan <eboominathan@gmail.com>
2024-07-14 12:04:36 +0530

    sign in page design complrted

--- src/app/(authentication)/auth/page.tsx
+++ src/app/(authentication)/auth/page.tsx
@@ -8,6 +8,7 @@ import { useForm } from "react-hook-form";
 
 import { BsSlack } from "react-icons/bs";
 import { FcGoogle } from "react-icons/fc";
+import { MdOutlineAutoAwesome } from "react-icons/md";
 import { RxGithubLogo } from "react-icons/rx";
 import { z } from "zod";
 
@@ -66,11 +67,12 @@ const AuthPage = () => {
           </Button>
         </div>
         <div>
-          <div className="flex items-center my-6">
-            <div className="mr-[10px] flex-1 border-t bg-neutral-300">
-             <Typography text='OR' variant='p' className='' />
-             <div className='mr-[10px] flex-1 border-t bg-neutral-300' />
+        <div className='flex items-center my-6'>
+            <div className='mr-[10px] flex-1 border-t bg-neutral-300' />
+            <Typography text='OR' variant='p' className=""/>
+            <div className='ml-[10px] flex-1 border-t bg-neutral-300' />
           </div>
+          
             {/* FORM  */}
             <Form {...form} >
               <form>
@@ -80,12 +82,24 @@ const AuthPage = () => {
                       <Input placeholder="name@work-email.com" {...field} />
                   </FormItem>
                 )}/>
-                <Button 
-                variant='secondary' 
-                className='bg-primary-dark hover-bg-primary-darker/90 w-full my-5 text-white '
-                 type='submit'>
-                    <Typography text='Sign in with Email' variant='p' className='' />
-                </Button> 
+              <Button
+                  variant='secondary'
+                  className='bg-primary-dark hover:bg-primary-dark/90 w-full my-5 text-white'
+                  type='submit'
+                >
+                  <Typography text='Sign in with Email' variant='p' className=""/>
+                </Button>
+
+                <div className='px-5 py-4 bg-gray-100 rounded-sm'>
+                  <div className='text-gray-500 flex items-center space-x-3'>
+                    <MdOutlineAutoAwesome />
+                    <Typography
+                      text='We will email you a magic link for a password-free sign-in'
+                      variant='p'
+                      className=""
+                    />
+                  </div>
+                </div>
                 </fieldset>
               </form>
             </Form>
@@ -93,8 +107,7 @@ const AuthPage = () => {
           </div>
         </div>
 
-      </div>
-    </div>
+      </div> 
   );
 };
 
