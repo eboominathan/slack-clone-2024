0a2e30c0e16d7e6fdd0fc78e442a75b2cad3c690
Boominathan <eboominathan@gmail.com>
2024-07-15 08:00:26 +0530

    create workspace in process

--- src/app/(authentication)/auth/page.tsx
+++ src/app/(authentication)/auth/page.tsx
@@ -1,14 +1,21 @@
-"use client"
+"use client";
 import { registerWithEmail } from "@/actions/resgister-with-email";
 import { Button } from "@/components/ui/button";
-import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import Typography from "@/components/ui/typography";
 import { supabaseBrowserClient } from "@/supabase/supabaseClient";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Provider } from "@supabase/supabase-js";
 import Link from "next/link";
-import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 import { set, useForm } from "react-hook-form";
 
 import { BsSlack } from "react-icons/bs";
@@ -18,18 +25,33 @@ import { RxGithubLogo } from "react-icons/rx";
 import { z } from "zod";
 
 const AuthPage = () => {
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+  const [isMounted, setIsMounted] = useState(false);
 
-    const [isAuthenticating, setIsAuthenticating] = useState(false); 
+  const router = useRouter();
+
+  useEffect(() => {
+    const getCurrentUser = async () => {
+      const {
+        data: { session },
+      } = await supabaseBrowserClient.auth.getSession();
+
+      if (session) {
+        router.push("/");
+      }
+    };
+    getCurrentUser();
+    setIsMounted(true);
+  }, [router]);
 
   const formSchema = z.object({
-      email: z.string().email().min(2,{message:"Email must be 2 characters"}),
-
+    email: z.string().email().min(2, { message: "Email must be 2 characters" }),
   });
   const form = useForm<z.infer<typeof formSchema>>({
-    resolver:zodResolver(formSchema),
-    defaultValues:{
-      email:'', 
-    }
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+    },
   });
 
   async function onSubmit(values: z.infer<typeof formSchema>) {
@@ -38,11 +60,11 @@ const AuthPage = () => {
     const { data, error } = JSON.parse(response);
     setIsAuthenticating(false);
     if (error) {
-      console.warn('Sign in error', error);
+      console.warn("Sign in error", error);
       return;
     }
   }
- 
+
   async function socialAuth(provider: Provider) {
     setIsAuthenticating(true);
     await supabaseBrowserClient.auth.signInWithOAuth({
@@ -54,103 +76,108 @@ const AuthPage = () => {
     setIsAuthenticating(false);
   }
 
-
-
   return (
-    <div className='min-h-screen  p-5 grid text-center place-content-center bg-white'>
-      <div className='max-w-[450px]'>
-        <div className='flex justify-center items-center gap-3 mb-4'>
+    <div className="min-h-screen  p-5 grid text-center place-content-center bg-white">
+      <div className="max-w-[450px]">
+        <div className="flex justify-center items-center gap-3 mb-4">
           <BsSlack size={30} />
-          <Typography
-            text='Slack Clone'
-            variant='h2'
-            className=''
-          />
+          <Typography text="Slack Clone" variant="h2" className="" />
         </div>
         <Typography
-            text='Sign in to Slack Clone'
-            variant='h2'
-            className='mb-3'
-          />
+          text="Sign in to Slack Clone"
+          variant="h2"
+          className="mb-3"
+        />
         <Typography
-            text='We suggest using the email address that you used at work'
-            variant='p'
-            className='opacity-90 mb-7'
-          />
+          text="We suggest using the email address that you used at work"
+          variant="p"
+          className="opacity-90 mb-7"
+        />
         <div className="flex flex-col space-y-4">
-          <Button 
-           disabled={isAuthenticating}
-           variant='outline' className="py-6 border-2 flex space-x-3" >
+          <Button
+            disabled={isAuthenticating}
+            variant="outline"
+            className="py-6 border-2 flex space-x-3"
+          >
             <FcGoogle size={30} />
             <Typography
-             className='text-xl'
-             text="Sign in with Google"
-             variant='p'
-             onClick={() => socialAuth('google')}            
-             ></Typography>             
+              className="text-xl"
+              text="Sign in with Google"
+              variant="p"
+              onClick={() => socialAuth("google")}
+            ></Typography>
           </Button>
-          <Button 
-          disabled={isAuthenticating}
-          variant='outline'
-           className="py-6 border-2 flex space-x-3">
-            <RxGithubLogo  size={30}/>
+          <Button
+            disabled={isAuthenticating}
+            variant="outline"
+            className="py-6 border-2 flex space-x-3"
+          >
+            <RxGithubLogo size={30} />
             <Typography
-             className='text-xl'
-             text="Sign in with Github"
-             variant='p'
-             onClick={() => socialAuth('github')}
-             ></Typography>             
+              className="text-xl"
+              text="Sign in with Github"
+              variant="p"
+              onClick={() => socialAuth("github")}
+            ></Typography>
           </Button>
         </div>
         <div>
-        <div className='flex items-center my-6'>
-            <div className='mr-[10px] flex-1 border-t bg-neutral-300' />
-            <Typography text='OR' variant='p' className=""/>
-            <div className='ml-[10px] flex-1 border-t bg-neutral-300' />
+          <div className="flex items-center my-6">
+            <div className="mr-[10px] flex-1 border-t bg-neutral-300" />
+            <Typography text="OR" variant="p" className="" />
+            <div className="ml-[10px] flex-1 border-t bg-neutral-300" />
           </div>
-          
-            {/* FORM  */}
-            <Form {...form} >
-              <form onSubmit={form.handleSubmit(onSubmit)}>
-                <fieldset disabled={isAuthenticating}>
+
+          {/* FORM  */}
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <fieldset disabled={isAuthenticating}>
                 <FormField
                   control={form.control}
-                  name='email'
+                  name="email"
                   render={({ field }) => (
                     <FormItem>
                       <FormControl>
-                        <Input placeholder='name@work-email.com' {...field} />
+                        <Input placeholder="name@work-email.com" {...field} />
                       </FormControl>
                       <FormMessage />
                     </FormItem>
                   )}
                 />
-              <Button
-                  variant='secondary'
-                  className='bg-primary-dark hover:bg-primary-dark/90 w-full my-5 text-white'
-                  type='submit'
+                <Button
+                  variant="secondary"
+                  className="bg-primary-dark hover:bg-primary-dark/90 w-full my-5 text-white"
+                  type="submit"
                 >
-                  <Typography text='Sign in with Email' variant='p' className=""/>
+                  <Typography
+                    text="Sign in with Email"
+                    variant="p"
+                    className=""
+                  />
                 </Button>
 
-                <div className='px-5 py-4 bg-gray-100 rounded-sm'>
-                  <div className='text-gray-500 flex items-center space-x-3'>
+                <div className="px-5 py-4 bg-gray-100 rounded-sm">
+                  <div className="text-gray-500 flex items-center space-x-3">
                     <MdOutlineAutoAwesome />
                     <Typography
-                      text='We will email you a magic link for a password-free sign-in'
-                      variant='p'
+                      text="We will email you a magic link for a password-free sign-in"
+                      variant="p"
                       className=""
                     />
                   </div>
                 </div>
-                </fieldset>
-              </form>
-            </Form>
-                
-          </div>
+              </fieldset>
+            </form>
+          </Form>
         </div>
-                <p className="text-gray-500 my-10">&copy; 2024 Developed By <Link href="https://linkedin.com/in/eboominathan">Boominathan Elango</Link></p>
-      </div> 
+      </div>
+      <p className="text-gray-500 my-10">
+        &copy; 2024 Developed By{" "}
+        <Link href="https://linkedin.com/in/eboominathan">
+          Boominathan Elango
+        </Link>
+      </p>
+    </div>
   );
 };
 
